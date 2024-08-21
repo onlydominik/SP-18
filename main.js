00476ae6f0974dc00fc1c0b3611f76af195dad14
Dominik <113696375+onlydominik@users.noreply.github.com>
2024-08-21 02:27:47 +0200

    Submit btn problem

--- main.js
+++ main.js
@@ -2,24 +2,58 @@
 const form = document.querySelector("#contact-form");
 const formData = new FormData(form);
 
+let firstNameIsCorrect = true;
+let lastNameIsCorrect = true;
+let emailIsCorrect = true;
+let textareaIsCorrect = false;
+let nowe;
 
-console.log(firstName);
-
-
-let msg = document.createElement("p");
-form.addEventListener("submit", (e) => {
-    if (firstName === "" || firstName == null) {
-        msg.innerHTML = "Podaj swoje imię!";
-        msg.classList.add("invalid-value-msg");
-     
-
-
-       // document.querySelector("#firstName").append(msg)
-
-    }
-e.preventDefault();
-})
-
+let msg ="";
 const handleFirstName = () => {
-    if(!firstName) console.log("Cos")
+    msg = firstName.dataset.err;
+    showErrMsg(firstNameIsCorrect, firstName, msg)
 }
+
+const handleLastName = () => {
+    msg = lastName.dataset.err;
+    showErrMsg(lastNameIsCorrect, lastName, msg)
+}
+
+const handleEmail = () => {
+    msg = email.dataset.err;
+    showErrMsg(emailIsCorrect, email, msg)
+}
+const handleTextarea = () => {
+    msg = textarea.dataset.err;
+    showErrMsg(textareaIsCorrect, textarea, msg)
+}
+
+
+const showErrMsg = (cos, elem, msg) => {  
+    document.querySelector(".btn").disabled = !wlaczone;
+    if(!elem.value) {
+        document.querySelector(".invalid-value-msg--" + elem.id).innerHTML = msg;
+        wlaczone = false;
+        return;
+    }  
+        document.querySelector(".invalid-value-msg--" + elem.id).innerHTML = "";
+     
+        wlaczone = true;
+
+      
+     
+}
+//$: wlaczone = firstNameIsCorrect && lastNameIsCorrect && emailIsCorrect && textareaIsCorrect;
+$: wlaczone = false;
+
+
+form.addEventListener("keydown", () => {
+ 
+    document.querySelector(".btn").disabled = !wlaczone;
+});
+
+
+
+
+
+
